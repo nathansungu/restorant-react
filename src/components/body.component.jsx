9a134e563ed59cbac4515d434d6e7e8b6ca66aff
nathansungu <nathanamudavi@gmail.com>
2025-06-19 06:05:28 +0300

    add discount to products

--- src/components/body.component.jsx
+++ src/components/body.component.jsx
@@ -3,10 +3,24 @@ import foodItems from "../fooditems";
 const BodyItem = ({ src, alt, name, price, discount, description }) => {
   return (
     <div className="item">
-      <img src={src} alt={alt} />
+      <span className="image-container">
+        <p className="discount" style={{display: discount>0? 'flex':'none'}}>{discount}%</p>        
+        <img src={src} alt={alt} />
+      </span>
       <p className="item-name">{name}</p>
       <p className="description">{description}</p>
-      <p>{discount >0 ?<><span className="price">{Math.ceil(price*(100 - discount)/100) } </span> <span className="originalprice">{price}</span></>:<span className="price">{price}</span>}</p>
+      <p>
+        {discount > 0 ? (
+          <>
+            <span className="price">
+              ${((price * (100 - discount)) / 100).toFixed(2)}{" "}
+            </span>{" "}
+          </>
+        ) : (
+          <span className="price">${price}</span>
+        )}
+      </p>
+      
     </div>
   );
 };
@@ -14,23 +28,21 @@ const BodyItem = ({ src, alt, name, price, discount, description }) => {
 const BodySection = () => {
   return (
     <>
-      
-        <div className="container">
-          {foodItems.map((listing, indx) => {
-            return (
-              <BodyItem
-                key={indx}
-                src={listing.scr}
-                alt={listing.alt}
-                name={listing.name}
-                price={listing.price}
-                discount={listing.discount}
-                description={listing.description}
-              />
-            );
-          })}
-        </div>
-      
+      <div className="container">
+        {foodItems.map((listing, indx) => {
+          return (
+            <BodyItem
+              key={indx}
+              src={listing.scr}
+              alt={listing.alt}
+              name={listing.name}
+              price={listing.price}
+              discount={listing.discount}
+              description={listing.description}
+            />
+          );
+        })}
+      </div>
     </>
   );
 };
--- src/fooditems.js
+++ src/fooditems.js
@@ -1,7 +1,7 @@
  const foodItems = [
     {name:"burger",description:"Consists of fillings—usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll.",price:40,alt:"burger",scr:"/burger.jpg", discount:5},
     {name:"dinner pork",description:"Your pork chops were a big hit, by the way.",price:60,alt:"pork",scr:"./dinner-pork.jpg", discount:10},
-    {name:"food plate",description:"A vibrant food plate featuring a balanced mix of colorful vegetables, tender proteins, hearty grains, and a drizzle of flavorful sauce for a delicious, wholesome meal. ",price:90,alt:"food plate",scr:"./food-plate.jpg", discount:32},
+    {name:"food plate",description:"A vibrant food plate featuring a balanced mix of colorful vegetables, tender proteins, hearty grains, and a drizzle of flavorful sauce. ",price:90,alt:"food plate",scr:"./food-plate.jpg", discount:32},
     {name:"grilled meat",description:"GTender grilled meat, perfectly seasoned, smoky aroma, juicy inside with charred edges.",price:50,alt:"griled meat",scr:"./grilled-meat.jpg",  discount:0},
     {name:"kabab",description:"Juicy, smoky kababs grilled to perfection with bold spices.",price:30,alt:"kabab",scr:"./kabab.jpg",  discount:0},
     {name:"korean dinner",description:"A cozy Korean diner serving sizzling, flavor-packed dishes like bulgogi, kimchi stew.",price:45,alt:"korean dinner",scr:"./korean-dinner.jpg",  discount:15},
